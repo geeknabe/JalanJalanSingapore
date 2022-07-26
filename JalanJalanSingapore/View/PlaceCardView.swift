a116673b2c96ff3af5a86e7491ee2883fe5e15d6
Mun Mun <munmun@sgcodecampus.com>
2022-07-26 11:22:24 +0800

    Commented out PlaceCardView.

--- JalanJalanSingapore/View/PlaceCardView.swift
+++ JalanJalanSingapore/View/PlaceCardView.swift
@@ -4,8 +4,25 @@ struct PlaceCardView: View {
     let place: Place
     
     var body: some View {
-        // Add code here
         Text("Hello world!")
+        
+//        ZStack {
+//            Image(place.image)
+//                .resizable()
+//                .scaledToFill()
+//        } // :ZSTACK
+//        .frame(
+//            minWidth: 0,
+//            maxWidth: .infinity,
+//            minHeight: 0,
+//            maxHeight: .infinity,
+//            alignment: .center
+//        )
+//        .cornerRadius(20)
+//        .overlay(
+//            PlaceCardDetailView(place: place),
+//            alignment: .bottomLeading
+//        )
     }
 }
 
